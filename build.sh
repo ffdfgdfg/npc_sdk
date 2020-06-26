1384807c28d0218ab4a8d6adc38e3712b0cb0cea
ffdfgdfg <heyingtingaaa@gmail.com>
2020-06-27 01:29:36 +0800

    change build.sh

--- build.sh
+++ build.sh
@@ -2,5 +2,9 @@ cd /go
 go get golang.org/x/mobile/cmd/gomobile
 gomobile init
 go get -d ehang.io/nps
-cd /app
+mkdir -p /go/src/github.com/ffdfgdfg/npc_sdk/npc
+cp -R /app/* /go/src/github.com/ffdfgdfg/npc_sdk/npc
+cd /go/src/github.com/ffdfgdfg/npc_sdk/npc
 gomobile bind github.com/ffdfgdfg/npc_sdk/npc
+cp npc.aar /app
+cp npc-sources.jar /app
