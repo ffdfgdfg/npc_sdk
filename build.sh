b095c2e011f813b28e56aeb4767de2d49a3cd50a
ffdfgdfg <heyingtingaaa@gmail.com>
2020-06-27 19:22:35 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,19 @@
 cd /go
 go get golang.org/x/mobile/cmd/gomobile
 gomobile init
-go get -d ehang.io/nps
-mkdir -p /go/src/github.com/ffdfgdfg/npc_sdk/npc
-cp -R /app/* /go/src/github.com/ffdfgdfg/npc_sdk/npc
-cd /go/src/github.com/ffdfgdfg/npc_sdk/npc
+echo "gomobile install finish"
+go get -d -v ehang.io/nps
+cd /go/src/ehang.io/nps
+go mod vendor
+cd vendor
+cp -R * /go/src
+cd ..
+rm -rf vendor
+echo "dependences install finish"
+mkdir -p /go/src/github.com/ffdfgdfg/npc_sdk
+cp -R /app/* /go/src/github.com/ffdfgdfg/npc_sdk
+cd /go/src/github.com/ffdfgdfg/npc_sdk
 gomobile bind github.com/ffdfgdfg/npc_sdk/npc
+echo "bind finish"
 cp npc.aar /app
 cp npc-sources.jar /app
